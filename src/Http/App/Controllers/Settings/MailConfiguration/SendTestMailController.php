037159eed490ab4fb28d170afaf109dcd0d9817a
Rias <hey@rias.be>
2021-03-24 10:06:17 +0100

    Add mailcoach header on test mail

--- src/Http/App/Controllers/Settings/MailConfiguration/SendTestMailController.php
+++ src/Http/App/Controllers/Settings/MailConfiguration/SendTestMailController.php
@@ -3,6 +3,7 @@
 namespace Spatie\MailcoachUi\Http\App\Controllers\Settings\MailConfiguration;
 
 use Exception;
+use Swift_Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Spatie\MailcoachUi\Mail\TestMail;
@@ -22,7 +23,14 @@ class SendTestMailController
         ]);
 
         try {
-            Mail::mailer('mailcoach')->to($request->to_email)->send(new TestMail($request->from_email, $request->to_email));
+            $mail = new TestMail($request->from_email, $request->to_email);
+            $mail->withSwiftMessage(function (Swift_Message $message) {
+                $message->getHeaders()->addTextHeader('X-MAILCOACH', 'true');
+            });
+
+            Mail::mailer('mailcoach')
+                ->to($request->to_email)
+                ->send($mail);
 
             flash()->success(__('A test mail has been sent to :email. Please check if it arrived.', ['email' => $request->to_email]));
         } catch (Exception $exception) {
