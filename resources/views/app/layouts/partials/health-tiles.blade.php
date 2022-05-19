6b89ba9ec308f35aceaeb5b00c5a49025a349615
Rias <hey@rias.be>
2022-05-19 09:40:04 +0200

    Add some color to the tile

--- resources/views/app/layouts/partials/health-tiles.blade.php
+++ resources/views/app/layouts/partials/health-tiles.blade.php
@@ -1,7 +1,7 @@
 @if ((! $mailConfigurationValid && ! request()->routeIs('mailConfiguration')) || (! $usesVapor && ! $horizonActive && \Composer\InstalledVersions::isInstalled("laravel/horizon")) || ! $queueConfig)
     @if (! request()->routeIs('mailConfiguration'))
         @if(! $mailConfigurationValid)
-            <x-mailcoach::tile class="bg-red-100" cols="2" icon="server">
+            <x-mailcoach::tile class="bg-gradient-to-r from-red-500 to-red-600 text-white rounded" cols="2" icon="server">
                 <x-slot:link><a class="underline" href="{{ route('mailConfiguration') }}" data-turbo="false">Mail configuration</a></x-slot:link>
                 {!! __('Your <strong>mail configuration</strong> is invalid.') !!}
             </x-mailcoach::tile>
@@ -10,14 +10,14 @@
 
 
     @if (! $queueConfig)
-        <x-mailcoach::tile class="bg-red-100" cols="2" icon="database">
+        <x-mailcoach::tile class="bg-gradient-to-r from-red-500 to-red-600 text-white rounded" cols="2" icon="database">
             <x-slot:link><a class="underline" href="https://spatie.be/docs/laravel-mailcoach" data-turbo="false">Docs</a></x-slot:link>
             {!! __('No valid <strong>queue connection</strong> found. Configure a queue connection with the <strong>mailcoach-redis</strong> key.') !!}
         </x-mailcoach::tile>
     @endif
 
     @if(! $usesVapor && ! $horizonActive && \Composer\InstalledVersions::isInstalled("laravel/horizon"))
-        <x-mailcoach::tile class="bg-red-100" cols="2" icon="database">
+        <x-mailcoach::tile class="bg-gradient-to-r from-red-500 to-red-600 text-white rounded" cols="2" icon="database">
             <x-slot:link><a class="underline" href="https://spatie.be/docs/laravel-mailcoach" data-turbo="false">Docs</a></x-slot:link>
             {!! __('<strong>Horizon</strong> is not active on your server.') !!}
         </x-mailcoach::tile>
