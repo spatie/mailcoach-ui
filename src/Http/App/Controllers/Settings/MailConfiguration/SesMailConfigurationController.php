edc1f2da0f4153ce5b7bace880a31ef5c87cb145
Freek Van der Herten <freek@spatie.be>
2022-05-27 11:40:28 +0200

    wip

--- src/Http/App/Controllers/Settings/MailConfiguration/SesMailConfigurationController.php
+++ src/Http/App/Controllers/Settings/MailConfiguration/SesMailConfigurationController.php
@@ -2,10 +2,14 @@
 
 namespace Spatie\MailcoachUi\Http\App\Controllers\Settings\MailConfiguration;
 
+use Spatie\Mailcoach\MainNavigation;
+
 class SesMailConfigurationController
 {
     public function __invoke()
     {
+        app(MainNavigation::class)->activeSection()->add('Ses setup wizard');
+
         return view('mailcoach-ui::app.drivers.campaigns.ses');
     }
 }
