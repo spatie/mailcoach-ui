512a0ebe349f698d3b840278a5a02ac9224185b2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-02 01:17:04 +0800

    Delete resources directory

--- resources/views/app/configuration/editor/partials/textarea.blade.php
+++
@@ -1,3 +0,0 @@
-<x-mailcoach::help>
-{{ __('This editor is a plain textarea field.') }}
-</x-mailcoach::help>
--- resources/views/mails/test.blade.php
+++
@@ -1,7 +0,0 @@
-@component('mailcoach::mails.layout.message')
-
-{{ __('Hi') }}
-
-{{ __('This is a test email sent from Mailcoach.') }}
-
-@endcomponent
