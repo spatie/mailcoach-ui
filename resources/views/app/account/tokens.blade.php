278c3f6c5d438300b11d9ef23dab4dd0389d7150
Rias <hey@rias.be>
2022-06-27 14:50:09 +0200

    Use alpine-clipboard

--- resources/views/app/account/tokens.blade.php
+++ resources/views/app/account/tokens.blade.php
@@ -44,31 +44,6 @@
                 <span class="cursor-pointer underline text-sm mr-4" @click="$clipboard('{{ $newToken }}'); $el.innerText = 'Copied!'">Copy to clipboard</span>
             </div>
         </x-mailcoach::help>
-        <script>
-            function copyToClipboard(textToCopy) {
-                // navigator clipboard api needs a secure context (https)
-                if (navigator.clipboard && window.isSecureContext) {
-                    // navigator clipboard api method'
-                    return navigator.clipboard.writeText(textToCopy);
-                } else {
-                    // text area method
-                    let textArea = document.createElement("textarea");
-                    textArea.value = textToCopy;
-                    // make the textarea out of viewport
-                    textArea.style.position = "fixed";
-                    textArea.style.left = "-999999px";
-                    textArea.style.top = "-999999px";
-                    document.body.appendChild(textArea);
-                    textArea.focus();
-                    textArea.select();
-                    return new Promise((res, rej) => {
-                        // here the magic happens
-                        document.execCommand('copy') ? res() : rej();
-                        textArea.remove();
-                    });
-                }
-            }
-        </script>
     @endif
 
     @if (count($tokens))
