07b6725ede6fa36931d4ac9837e5a9f498719429
Willem Van Bockstal <willem@spatie.be>
2022-07-08 14:31:24 +0200

    Alt design wip

--- resources/views/app/configuration/editor/edit.blade.php
+++ resources/views/app/configuration/editor/edit.blade.php
@@ -12,35 +12,45 @@
 >
     @csrf
 
-    <x-mailcoach::fieldset :legend="__('Content editor')">
-        <x-mailcoach::select-field
-            :label="__('Editor')"
-            name="contentEditor"
-            x-model="contentEditor"
-            :options="$contentEditorOptions"
-        />
-    </x-mailcoach::fieldset>
-
-    <x-mailcoach::fieldset :legend="__('Template editor')">
-        <x-mailcoach::select-field
-            :label="__('Editor')"
-            name="templateEditor"
-            x-model="templateEditor"
-            :options="$templateEditorOptions"
-        />
-    </x-mailcoach::fieldset>
-
-    @foreach(config('mailcoach-ui.editors') as $editor)
-        @continue($contentEditor !== $editor::label() && $templateEditor !== $editor::label())
-
-        <x-mailcoach::fieldset :legend="$editor::label()">
-            <div class="form-grid">
-                @includeIf($editor::settingsPartial())
-            </div>
+        <x-mailcoach::fieldset :legend="__('Content editor')">
+            <x-mailcoach::select-field
+                    name="contentEditor"
+                    x-model="contentEditor"
+                    :options="$contentEditorOptions"
+                />
+            
+                @foreach(config('mailcoach-ui.editors') as $editor)
+                @if($contentEditor === $editor::label())
+                        <div class="form-grid">
+                            @includeIf($editor::settingsPartial())
+                        </div>
+                @endif
+            @endforeach
         </x-mailcoach::fieldset>
-    @endforeach
 
-    <div class="form-buttons">
-        <x-mailcoach::button :label="__('Save')"/>
-    </div>
+        <x-mailcoach::fieldset :legend="__('Template editor')">
+            <x-mailcoach::select-field
+                name="templateEditor"
+                x-model="templateEditor"
+                :options="$templateEditorOptions"
+            />
+            @foreach(config('mailcoach-ui.editors') as $editor)
+                @if($templateEditor === $editor::label())
+                    @if($templateEditor === $contentEditor)
+                        <x-mailcoach::info>
+                            {{ __('Uses same settings as the content editor.') }}
+                        </x-mailcoach::info>
+                    @else
+                    <div class="form-grid">
+                        @includeIf($editor::settingsPartial())
+                    </div>
+                    @endif
+                @endif
+            @endforeach
+        </x-mailcoach::fieldset>
+
+        <div class="form-buttons">
+            <x-mailcoach::button :label="__('Save')"/>
+        </div>
+
 </form>
